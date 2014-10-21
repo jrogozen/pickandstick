92b726ef6780bf15b5b7f47ea6272c5f81a09bf8
Jon Rogozen <jon.rogozen@gmail.com>
2014-10-21 15:44:41 -0700

    add user profiles for non logged in members

--- app/assets/javascripts/angular/controllers/user-controller.js
+++ app/assets/javascripts/angular/controllers/user-controller.js
@@ -1,20 +1,26 @@
-app.controller('UserController', ['$scope', '$location', 'User', 'Session',
-  function($scope, $location, User, Session) {
+app.controller('UserController', ['$scope', '$location', '$routeParams', 'User', 'Session',
+  function($scope, $location, $routeParams, User, Session) {
     $scope.users = User.query();
 
     Session.getSessionData(function(data) {
-      $scope.user = User.get({id: data.user_id});
+      if (data.user_id) {
+        $scope.currentUser = User.get({id: data.user_id});
+      }
     });
+
+    if ($routeParams.id) {
+      $scope.user = User.get({id: $routeParams.id});
+    }
 
     $scope.newUser = function(data) {
       User.save({user: data}, function(success){
-        $location.path('/users');
+        $location.path('/users/profile');
       });
     };
 
     $scope.loginUser = function(data) {
       User.login({user: data}, function(success) {
-        $location.path('/users/' + success.id);
+        $location.path('/users/profile');
       });
     };
 
